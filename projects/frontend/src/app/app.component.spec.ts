1adad9b37a4315b01a4d5a6c15c3ab9258f8a3dc
Doug Parker <dgp1130@users.noreply.github.com>
2022-08-25 20:01:08 -0700

    Removes generated UI and replaces with minimal "Hello, World!" message.

--- projects/frontend/src/app/app.component.spec.ts
+++ projects/frontend/src/app/app.component.spec.ts
@@ -20,16 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
-  });
-
-  it('should render title', () => {
+  it('should say hello', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, World!');
   });
 });
